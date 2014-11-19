5748dee07ddad34b454023ce4775d15bf284946e
Noah Whaley <nwhaley@purdue.edu>
2014-11-19 12:50:07 -0500

    done now hopefully

--- calc.py
+++ calc.py
@@ -71,8 +71,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
-		elif (op == "^"):
-			print "Power: ", exp(a, b)
+        elif (op == "^"):
+            print "Power: ", exp(a, b)
         elif (op == "%"):
             print "Modulus: ", mod(a, b)
         else:
